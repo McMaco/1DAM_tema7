d6d49309e7d24c64ec49f85d6c6795e68609b1f1
McMaco <david.mccvic@educa.jcyl.es>
2019-01-16 18:36:07 +0100

    versión 0.4

--- src/sopa_de_letras.java
+++ src/sopa_de_letras.java
@@ -8,238 +8,260 @@
 import java.util.Scanner;
 
 public class sopa_de_letras {
+
+	public static char s[][] = new char[15][15];
 	
-	public static char sopa[][] = new char [15][15];
-	
-	/*MÉTODO--> crear
-	 * 
-	 * Entradas--> Éste método no tiene entradas
-	 * 
-	 * Salidas--> El método no devuelve nada
-	 * 
-	 * Descripción--> Éste método tiene como función crear una sopa de letras aleatorias
-	 */
-	public static void crear() {
-		
-		int f, c;
-		
-		for(f=0; f<15; f++) {
-			for(c=0; c<15; c++) {
-				sopa[f][c] = (char) (Math.random()* 26 + 'A'); 
-			}
-		}
-		System.out.println();
-	}
-	
-	/*MÉTODO--> colocar
-	 * 
-	 * Entradas--> Éste método no tiene entradas
-	 * 
-	 * Salidas--> El método no devuelve nada
-	 * 
-	 * Descripción--> Éste método tiene como función mostrar y recoger las opciones que da el juego
+	static Scanner t = new Scanner(System.in);
+
+	/*METODO: public static void colocar().
+	 *
+	 * DESCRIPCIÓN: En este metodo lo que hace es que pide la palabra y te la coloca
+	 * aleatoriamente dentro de la sopa de letras.
 	 */
 	public static void colocar() {
-		
-		Scanner t = new Scanner(System.in);
-		int f, c, i, opc, n=0, j=0;
+
+		int f, c, i, k, l, n = 0;
 		String palabra = null;
-		System.out.println("INTRODUCCION DE PALABRAS: ");
-		
+
+		System.out.println("INTRODUCE LAS PALABRAS DE LA SOPA DE LETRAS");
+		System.out.println();
 		do {
-			boolean flag=true;
-			boolean print=true;
+			boolean ok = true;
+			boolean print = true;
+
 			try {
 				do {
-					System.out.print((j+1)+": ");
+					System.out.print((n + 1) + ": ");
 					palabra = t.next();
 					palabra = palabra.toUpperCase();
-				}while((palabra.length()<3)||(palabra.length()>15)||(palabra.matches(".*[^A-Z].*")));
-				j++;
-			}
-			catch(Exception e) {
-				t.nextInt();
+				} while ((palabra.length() < 3) || (palabra.length() > 15) || (palabra.matches(".*[^A-Z].*")));
+				
+				System.out.println(palabra);
+				
+			} catch (Exception e) {
+				t.next();
 			}
 			n++;
-			
-			System.out.println(palabra);
-			
-			char v [] = palabra.toCharArray();  
-			
+
+			char v[] = palabra.toCharArray();
+
+			for (i = 0; i < palabra.length(); i++) {
+
+			}
+
 			do {
-				flag=true;
-				print=true;
+				ok = true;
+				print = true;
 				try {
-					
-					f = (int) (Math.random() * 14);
-					c = (int) (Math.random() * 14);
-					sopa[f][c] = v[0];
-				
-					opc = (int) (Math.random()*(8-1)+1);
-					switch(opc) {
-						
-						case 1: //DIAGONAL DE ARRIBA A ABAJO Y DE IZQUIERDA A DERECHA
-							
-							if(((f+(palabra.length()-1))>15)||((c+(palabra.length()-1))>15))       flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f+i][c+i] = v[i]; 
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-								}
-							}
-							
-						break;
-						
-						case 2: //DIAGONAL DE ABAJO A ARRIBA Y DE DERECHA A IZQUIERDA
-							if(((f-(palabra.length()-1))<0)||((c-(palabra.length()-1))<0))       flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f-i][c-i] = v[i]; 
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-								}
-							}
-							
-						break;
-						
-						case 3: //DIAGONAL DE ARRIBA A ABAJO Y DE DERECHA A IZQUIERDA
-							if(((f+(palabra.length()-1))>15)||((c-(palabra.length()-1))<0))       flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f+i][c-i] = v[i]; 
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-								}
-							}
-						break;
-						
-						case 4: //DIAGONAL DE ABAJO A ARRIBA Y DE IZQUIERDA A DERECHA
-							if(((f-(palabra.length()-1))<0)||((c+(palabra.length()-1))>15))       flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f-i][c+i] = v[i]; 
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-								}
-							}
-						break;
-						
-						case 5: //HORIZONTAL DE IZQUIERDA A DERECHA
-							if((c+(palabra.length()-1))>15)       flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f][c+i] = v[i]; 
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-								}
-							}
-						break;
-						
-						case 6: //HORIZONTAL DE DERECHA A IZQUIERDA
-							if((c-(palabra.length()-1))<0)     flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f][c-i] = v[i];
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-								}
-							}
-						break;
-						
-						case 7: //VERTICAL DE ARRIBA A ABAJO
-							if((f+(palabra.length()-1))>15)       flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f+i][c] = v[i];
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-								}
-							}
-						break;
-						
-						case 8: //VERTICAL DE ABAJO A ARRIBA
-							if((f-(palabra.length()-1))<0)       flag=false;
-							else {
-								for(i=1; i<palabra.length(); i++) {
-									sopa[f-i][c] = v[i]; 
-									if(sopa[f+i][c+i]==v[i]) {
-										print=true;
-									}
-									else {
-										print=false;
-									}
-									
-									if((f-i)<0)     flag=false;
-								}
-							}
-						break;
-		
+					f = (int) (Math.random() * 15);
+					c = (int) (Math.random() * 15);
+
+					s[f][c] = v[0];
+
+					if (s[f][c] == v[0]) {
+
+					} else {
+						print = false;
 					}
+
+					int opc = (int) (Math.random() * (8 - 1) + 1);
+
+					switch (opc) {
+
+						case 1: // Diagonal a la derecha hacia abajo
+	
+							if (((f + (palabra.length() - 1)) > 15) || ((c + (palabra.length() - 1)) > 15)) {
+								ok = false;
+							} else {
+	
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f + j][c + j] = v[j];
+	
+									if (s[f + j][c + j] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+	
+						break;
+	
+						case 2:// Vertical hacia abajo.
+	
+							if ((f + (palabra.length() - 1)) > 15) {
+								ok = false;
+							} else {
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f + j][c] = v[j];
+	
+									if (s[f + j][c] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+	
+						break;
+	
+						case 3:// Horizontal de izquierda a derecha.
+	
+							if ((c + (palabra.length() - 1)) > 15) {
+								ok = false;
+							} else {
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f][c + j] = v[j];
+	
+									if (s[f][c + j] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+	
+						break;
+	
+						case 4:// Vertical hacia arriba.
+	
+							if ((f - (palabra.length() - 1)) < 0) {
+								ok = false;
+							} else {
+	
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f - j][c] = v[j];
+	
+									if (s[f - j][c] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+	
+						break;
+	
+						case 5:// Horizontal hacia la izquierda.
+	
+							if ((c - (palabra.length() - 1)) < 0) {
+								ok = false;
+							} else {
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f][c - j] = v[j];
+	
+									if (s[f][c - j] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+							
+						break;
+	
+						case 6: // Diagonal hacia la derecha y hacia arriba.
+	
+							if (((f - (palabra.length() - 1)) < 0) || ((c + (palabra.length() - 1)) > 15)) {
+								ok = false;
+							} else {
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f - j][c + j] = v[j];
+	
+									if (s[f - j][c + j] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+							
+						break;
+	
+						case 7: // Diagonal hacia la izquierda y hacia abajo.
+	
+							if (((f + palabra.length() - 1) > 15) || (c - (palabra.length() - 1) < 0)) {
+								ok = false;
+							} else {
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f + j][c - j] = v[j];
+	
+									if (s[f - j][c] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+	
+						break;
+	
+						case 8: // Diagonal hacia la izquierda y hacia arriba.
+	
+							if (((f - palabra.length() - 1) < 0) || ((c - palabra.length() - 1) < 0)) {
+								ok = true;
+							} else {
+								for (int j = 1; j < palabra.length(); j++) {
+									s[f - j][c - j] = v[j];
+	
+									if (s[f - j][c - j] == v[j]) {
+	
+									} else {
+										print = false;
+									}
+								}
+							}
+	
+						break;
+					}
+				} catch (Exception e) {
+
 				}
-				catch(Exception e) {
-				
-				}
-			}while((flag==false)||(print==false));
-			
-		}while(n<10);
-			
-		for(f=0; f<15; f++) {
-			for(c=0; c<15; c++) {
-				System.out.print(" " + sopa[f][c]);
+			} while ((ok == false) || (print == false));
+
+		} while (n < 10);
+
+	}
+
+	/* METODO: public static void crear_pintar(). 
+	 * ENTRADAS: 
+	 * SALIDAS: 
+	 * DESCRIPCIÓN:
+	 */
+
+	public static void crear_pintar() {
+		int f, c;
+
+		for (f = 0; f < 15; f++) {
+			for (c = 0; c < 15; c++) {
+				s[f][c] = (char) (Math.random() * 26 + 'a');
+
+			}
+		}
+
+		colocar();
+
+		for (f = 0; f < 15; f++) {
+			for (c = 0; c < 15; c++) {
+
+				System.out.print(" " + s[f][c]);
 			}
 			System.out.println();
 		}
-		
-	
-	}
-	
 
-	/*MÉTODO--> main
-	 *
-	 * Entradas--> Éste método no tiene entradas
+	}
+
+	/*
 	 * 
-	 * Salidas--> El método no devuelve nada
-	 * 
-	 * Descripción--> Éste método tiene como función ejecutar el programa y hacer las llamadas a los diferentes métodos
 	 */
-	public static void main (String [] args) {
-		
+	public static void main(String[] args) {
+		int f, c;
 		System.out.println("--------------");
 		System.out.println("SOPA DE LETRAS");
 		System.out.println("--------------");
-		System.out.println();
-		
-		crear();
-		colocar();
+
+		crear_pintar();
+
 	}
 
 }
